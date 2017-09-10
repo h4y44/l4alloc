023771859831eedb91593adf1e5fea1908a7d75e
l4 <l4.foss@gmail.com>
2017-09-11 02:59:58 +0700

    fix test build

--- test/free.c
+++ test/free.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main() {
-	void 
-
+	void *s = l4malloc(100);
+	l4free(s);
+	return 0;
 }
--- test/malloc.c
+++ test/malloc.c
@@ -6,5 +6,6 @@ int main() {
 	strcpy(s, "Hello heap!");
 	char *s2 = l4malloc(100);
 	puts(s);
+	print_trace(s);
 	return 0;
 }
