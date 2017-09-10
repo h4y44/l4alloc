676bc6a58ff9cfd76b8039d6e2d96cab3f62768b
l4 <l4.foss@gmail.com>
2017-09-10 21:40:01 +0700

    add test

---
+++ test/free.c
@@ -0,0 +1,7 @@
+#include "../l4alloc.h"
+#include <stdio.h>
+
+int main() {
+	void 
+
+}
---
+++ test/malloc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../l4alloc.h"
+
+int main() {
+	char *s = l4malloc(100);
+	strcpy(s, "Hello heap!");
+	char *s2 = l4malloc(100);
+	puts(s);
+	return 0;
+}
